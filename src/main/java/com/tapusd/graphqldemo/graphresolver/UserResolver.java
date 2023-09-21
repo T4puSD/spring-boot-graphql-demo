d0001101382cf2f900367449d0f3604fc6c283b0
tapusd <tapu.alphacommerz@gmail.com>
2023-09-21 09:45:38 +0600

    add query resolver for user by id

--- src/main/java/com/tapusd/graphqldemo/graphresolver/UserResolver.java
+++ src/main/java/com/tapusd/graphqldemo/graphresolver/UserResolver.java
@@ -2,6 +2,7 @@ package com.tapusd.graphqldemo.graphresolver;
 
 import com.tapusd.graphqldemo.domain.User;
 import com.tapusd.graphqldemo.service.UserService;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -19,6 +20,12 @@ public class UserResolver {
     @QueryMapping
     public List<User> users() {
         return userService.findAll();
+    }
+
+    @QueryMapping
+    public User user(@Argument Long id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found by id"));
     }
 
 
