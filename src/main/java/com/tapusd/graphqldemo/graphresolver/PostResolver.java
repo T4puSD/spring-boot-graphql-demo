eea92c2d3bf6f38dc83d5195bac35ab99f2cf85b
t4pusd <sdtapusd20@gmail.com>
2023-09-22 22:02:57 +0600

    add post resolver

--- src/main/java/com/tapusd/graphqldemo/graphresolver/PostResolver.java
+++ src/main/java/com/tapusd/graphqldemo/graphresolver/PostResolver.java
@@ -2,6 +2,7 @@ package com.tapusd.graphqldemo.graphresolver;
 
 import com.tapusd.graphqldemo.domain.Post;
 import com.tapusd.graphqldemo.service.PostService;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -19,5 +20,11 @@ public class PostResolver {
     @QueryMapping
     public List<Post> posts() {
         return postService.findAll();
+    }
+
+    @QueryMapping
+    public Post post(@Argument Long id) {
+        return postService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found with provided id!"));
     }
 }
