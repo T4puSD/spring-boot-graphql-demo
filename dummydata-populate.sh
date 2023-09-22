01ab319865651c5c3309f24074010fe7a5834821
t4pusd <sdtapusd20@gmail.com>
2023-09-22 22:16:56 +0600

    update dummy data

--- dummydata-populate.sh
+++ dummydata-populate.sh
@@ -8,7 +8,9 @@ curl -X POST http://localhost:8080/users -H "Content-Type: application/json" -d 
 ## Posts
 curl -X POST http://localhost:8080/posts -H "Content-Type: application/json" -d '{"slug": "the-dummy-post", "title": "The Dummy Post", "description": "THis is just a dummy post", "authorId": "1"}'
 
-curl -X POST http://localhost:8080/posts -H "Content-Type: application/json" -d '{"slug": "the-nvim-comparator", "title": "The Nvim Comparator of Future", "description": "Finally some comparator with futurestic API emerges in the NeoVim plugin repository. Really existed to try it out", "authorId": "2"}'
+curl -X POST http://localhost:8080/posts -H "Content-Type: application/json" -d '{"slug": "the-nvim-comparator", "title": "The Nvim Comparator of Future", "description": "Finally some comparator with futuristic API emerges in the NeoVim plugin repository. Really existed to try it out", "authorId": "2"}'
+
+curl -X POST http://localhost:8080/posts -H "Content-Type: application/json" -d '{"slug": "caddy", "caddy-as-ftp-server": "Caddy As FTP Server", "description": "Caddy is a fantastic front server! I really like it for its versatility but i use it mostly for serving files over ftp in my local network and i really like it. Give it a try yourself i hop you will like it.", "authorId": "2"}'
 
 
 ## Comments
